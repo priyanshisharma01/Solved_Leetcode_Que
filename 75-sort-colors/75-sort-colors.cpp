d10c0d4f6dc4d35c5d719c6eb7c13be92c5d12a0
PRIYANSHI SHARMA <78258971+priyanshisharma01@users.noreply.github.com>
2022-06-22 00:30:42 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (70.34%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        vector<int> r;
-        vector<int> w;
-        vector<int> b;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==0)
-            {
-                r.push_back(nums[i]);
-            }
-             else if(nums[i]==1)
-            {
-                w.push_back(nums[i]);
-            }
-            else
-            {
-                b.push_back(nums[i]);
-            }
+        map <int, int> m;
+        for(int i = 0; i < nums.size(); i++){
+            m[nums[i]]++;
         }
-        r.insert(r.end(),w.begin(),w.end());
-        r.insert(r.end(),b.begin(),b.end());
-        nums =r;
-        return;
+        int red = m[0];
+        int white = m[1];
+        int blue = m[2];
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(i < red)
+                nums[i] = 0;
+            else if(i >= red+white)
+                nums[i] = 2;
+            else
+                nums[i] = 1;
+        }
     }
 };
