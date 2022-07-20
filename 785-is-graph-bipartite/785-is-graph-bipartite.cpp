3603b76d4c8d6959ffbfbd6c76a00a6bf4008858
PRIYANSHI SHARMA <78258971+priyanshisharma01@users.noreply.github.com>
2022-07-21 00:02:54 +0530

    Time: 50 ms (27.80%), Space: 13.5 MB (83.52%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,3 +1,84 @@
+class Solution 
+{
+public:
+    bool helper(vector<vector<int>>&graph,vector<int>&colour,int curr)
+    {
+        for(auto it:graph[curr])
+        {
+            if(colour[it]==colour[curr])
+            {
+                return false;
+            }
+            else if(colour[it]==0)
+            {
+                colour[it]=-colour[curr];
+                if(!helper(graph,colour,it))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) 
+    {
+        int n=graph.size();
+        vector<int>colour(n,0);
+        for(int i=0;i<n;i++)
+        {
+            if(colour[i]==0) 
+            {
+                colour[i]=-1;
+                if(!helper(graph,colour,i))
+                    return false;
+            }
+        }
+        return true;
+    }
+};
+    /*
+    bool dfs(vector<vector<int>>& graph,vector<int>color,int currnode,int currcolor)
+    {
+        bool ans=true;
+        
+        color[currnode]=currcolor;
+        for(int i=0;i<graph[currnode].size();i++)
+        {
+            if(graph[currnode][i]==0)
+            {
+                color[graph[currnode][i]] = -1*color[currnode];
+                 dfs(graph,color,graph[currnode][i],color[graph[currnode][i]] );
+            }
+            else if(color[graph[currnode][i]] == color[currnode]){
+                    return false;}
+                
+        }
+        return ans;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        bool ans=true;
+        int n=graph.size();
+        vector<int> color(n,0);
+        for(int i=0;i<graph.size();i++)
+        {
+            if(color[i]==0)
+            {
+                 ans= (ans && dfs(graph,color,i,-1));
+                if(ans==false)
+                {
+                    break;
+                }
+            }
+
+        }
+        return ans;
+    }
+};
+*/
+
+
+//bfs
+/*
 class Solution {
 public:
     bool isBipartiteHelper(vector<vector<int>>& graph,vector<int>color,int currnode) {
@@ -15,7 +96,7 @@ public:
                 if(color[graph[frontNode][i]] == 0){
                     
                     q.push(graph[frontNode][i]);
-                    color[graph[frontNode][i]] = -1*color[frontNode];
+                    
                     
                 }else if(color[graph[frontNode][i]] == color[frontNode]){
                     return false;
@@ -48,3 +129,4 @@ public:
         return ans;
     }
 };
+*/
