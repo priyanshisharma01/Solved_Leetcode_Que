a967170c91d817ea75e86e8b21cbd3131e40aba0
PRIYANSHI SHARMA <78258971+priyanshisharma01@users.noreply.github.com>
2022-07-22 03:26:05 +0530

    Time: 5 ms (35.29%), Space: 7.5 MB (17.76%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -1,30 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* reverseBetween(ListNode* head, int m, int n) 
-{
-	ListNode *start, *h, *prev = nullptr;
-
-	for(h = head; n--; m--)
-		if(m>0)
-			start = exchange(prev, exchange(h, h->next));
-		else
-			prev  = exchange(h, exchange(h->next, prev));
-
-    if(start)
-        start->next->next = h, start->next = prev;
-    else
-        head->next = h, head = prev;
-
-	return head;
-}
+    ListNode* reverseBetween(ListNode* head, int L, int R) {
+        ListNode*dummy=new ListNode(),*prev=dummy,*left=head,*right=head,*forw=head->next;
+        dummy->next=head;//we have declared dummy variable and initialize its next to head
+		
+        int dif=(R-L);//we have stored the difference between R and L
+        while(--L){//we are traversing to the left node 
+            prev=left;
+            left=forw;
+            right=forw;
+            forw=forw->next;
+        }
+        
+        while(dif--){//after we reach the left node we will start reversing the linked list
+            ListNode*temp=forw->next;
+            forw->next=right;
+            right=forw;
+            forw=temp;
+        }
+        prev->next=right;
+        left->next=forw;
+        return dummy->next;
+    }
 };
