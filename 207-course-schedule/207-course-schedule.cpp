0b6c5ea4997d18653342417e1599ea38c8a1bca3
PRIYANSHI SHARMA <78258971+priyanshisharma01@users.noreply.github.com>
2022-07-22 00:00:00 +0530

    Time: 31 ms (60.83%), Space: 14.2 MB (45.69%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -21,8 +21,58 @@ bool canFinish(int n, vector<vector<int>>& prerequisites) {
 	}
 	vector<int> vis(n, 0);
 	for (int i = 0; i < n; ++i) {
-		if (isCycle(g, i, vis)) return false;
+        if(vis[i]==0)
+        {
+            if (isCycle(g, i, vis)) return false;
+        }
+		
 	}
 	return true;
 }
 };
+
+
+
+
+/*class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<int> in_degree(n, 0);
+        vector<int> adj[n];
+        
+        for(auto p: prerequisites)
+            adj[p[1]].push_back(p[0]);
+        
+        for(auto p: prerequisites)
+            in_degree[p[0]]++;
+        
+        queue<int> q;
+        
+        // Implementing Kahn's algo
+        for(int i = 0; i < n; ++i) {
+            if(in_degree[i] == 0)
+                q.push(i);
+        }
+        
+        int count = 0;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            count++;
+            
+            for(auto it: adj[node]) {
+                if(in_degree[it] != 0)
+                    in_degree[it]--;
+                if(in_degree[it] == 0)
+                    q.push(it);
+            }
+        }
+        
+        if(count == n)
+            return true;
+        
+        return false;
+        
+    }
+};*/
