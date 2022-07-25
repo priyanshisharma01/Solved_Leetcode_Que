1f5591b1bbda20fbf2e1ccb9debc68d4f15b3114
PRIYANSHI SHARMA <78258971+priyanshisharma01@users.noreply.github.com>
2022-07-26 02:24:49 +0530

    Time: 11 ms (68.40%), Space: 13.6 MB (63.15%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,75 +1,37 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int i=0;
-        int j=nums.size()-1;
-        vector<int> ans;
-        if(nums.size()==1 && nums[0]==target)
+        int start=0,end=nums.size()-1,res=-1;
+        vector<int>ans;
+        while(start<=end)
         {
-            ans.push_back(0);
-             ans.push_back(0);
-            return ans;
-        }
-        else if(nums.size()==1)
-        {
-            ans.push_back(-1);
-             ans.push_back(-1);
-            return ans;
-        }
-        if(i == j)
-        {
-            return ans;
-        }
-        bool flag =false;
-        bool ifp=false;
-        int idx;
-        int jdx;
-        bool jf=false;
-        while(i<=j)
-        {
-           if(nums[i]==nums[j] && nums[i]==target)
-           {
-               ans.push_back(i);
-               ans.push_back(j);
-               return ans;
-           }
-            else if(nums[i]<target && nums[j]==target)
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target)
             {
-                i++;
-                flag=true;
-                jf=true;
-                jdx=j;
+                res=mid;
+                end=mid-1;
             }
-            else if(nums[j]>target && nums[i]==target)
-            {
-                j--;
-                flag=true;
-                ifp=true;
-                idx=i;
-                
-            }
+            else if(target<nums[mid])
+                end=mid-1;
             else
+                start=mid+1;   
+        }
+        ans.push_back(res);
+        start=0,end=nums.size()-1,res=-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target)
             {
-                 i++;
-                 j--;
-                flag=false;
+                res=mid;
+                start=mid+1;
             }
-            
+            else if(target<nums[mid])
+                end=mid-1;
+            else
+                start=mid+1;   
         }
-        if(flag==false)
-        {
-            ans.push_back(-1);
-             ans.push_back(-1);
-        }else if(ifp)
-        {
-             ans.push_back(idx);
-             ans.push_back(idx);
-        }
-        else if(jf)
-        {
-             ans.push_back(jdx);
-             ans.push_back(jdx);
-        }
+        ans.push_back(res);
         return ans;
     }
 };
