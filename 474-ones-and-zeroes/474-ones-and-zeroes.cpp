e95cb05c9265d4064485207a97f18f9177062b24
PRIYANSHI SHARMA <78258971+priyanshisharma01@users.noreply.github.com>
2022-07-27 14:35:03 +0530

    Time: 533 ms (29.07%), Space: 32.5 MB (47.46%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -28,3 +28,60 @@ public:
         return dp[n][m][len];
     }
 };
+
+
+/*#Method 1: Recursion + Memoization (top-down dp)
+
+class Solution {
+public:
+    int dp[601][101][101];
+    int fun(int i,int m, int n, vector<string> &s){
+	   // base case
+        if(i >= s.size() || n<0 || m<0 ){
+            return 0;
+        }
+		// simply return the value if previously calculated
+        if(dp[i][m][n] != -1)return dp[i][m][n];
+        int c0 = count(s[i].begin(),s[i].end(),'0');
+        int c1 = count(s[i].begin(),s[i].end(),'1');
+		// if we can take this string , then ans_till_here = max(take it , avoid it)
+        if(m-c0 >= 0 && n-c1 >= 0 )
+        return dp[i][m][n] = max(fun(i+1,m-c0,n-c1,s)+1, fun(i+1,m,n,s));
+		//otherwise avoid it
+        else return dp[i][m][n] = fun(i+1,m,n,s);
+    }
+    int findMaxForm(vector<string>& s, int m, int n) {
+        int sz = s.size();
+        memset(dp,-1,sizeof(dp));
+        return fun(0,m,n,s);
+    }
+};
+#Method 2: Tabulation(bottom-up dp)
+
+class Solution {
+public:
+ 
+    int findMaxForm(vector<string>& s, int m, int n) {
+        int sz = s.size();
+        int dp[m+1][n+1];
+        memset(dp,0,sizeof(dp));
+        int cnt[sz+1][2];
+        for(int i=0;i<sz;i++){
+            int c0 = count(s[i].begin(), s[i].end(),'0');
+            int c1 = count(s[i].begin(), s[i].end(),'1');
+            cnt[i+1][0]= c0;
+            cnt[i+1][1]=c1;
+        }
+        for(int i=1;i <= sz;i++){
+            for(int j=m;j>=0;j--){
+                for(int k=n;k>=0;k--){
+                    
+                    if((j-cnt[i][0]) >=0 && (k-cnt[i][1]) >= 0){
+                        dp[j][k] = max(dp[j][k], dp[j-cnt[i][0]][k-cnt[i][1]]+1);
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};*/
