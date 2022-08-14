086cc031213947422c34563f55f7ae9cfe89063f
PRIYANSHI SHARMA <78258971+priyanshisharma01@users.noreply.github.com>
2022-08-14 15:51:57 +0530

    Time: 9 ms (96.35%), Space: 6.7 MB (100.00%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
-     string longestPalindrome(string s) 
-{   
-    int len = s.size();
-    int dp[len][len];
-    memset(dp,0,sizeof(dp));
-    int end=1;
-    int start=0;
-	
-    for(int i=0;i<len;i++)
-    {
-        dp[i][i] = 1;
-    }
-    for(int i=0;i<len-1;i++)
-    {
-        if(s[i]==s[i+1])
-        { dp[i][i+1]=1;start=i;end=2;}
-    }
-    
-    for(int j=2;j<len;j++)
-    {
-        for(int i=0;i< len-j;i++)
-        {  
-            int left=i; //start point
-            int right = i+j;  //ending point
-            
-            if(dp[left+1][right-1]==1 && s[left]==s[right]) 
-            {
-                dp[left][right]=1; start=i; end=j+1; 
-            }        
+    string longestPalindrome(string &s) {
+        int ansl=0;
+        int ansr=0;
+        int left=0;
+        int right=0;
+        for(int j=0;j<s.length();j++){
+
+            if(2*(s.length()-j)-1<=ansr-ansl+1){
+                continue;
+            }
+            left=j-1;
+            right=j+1;
+            while(left>=0 && right<s.length() && s[left]==s[right]){
+                
+                left--;
+                right++;
+                
+            }
+            if(right-left-1>=ansr-ansl+1){
+               
+                ansl=left+1;
+                ansr=right-1;  
+           }
         }
+         for(int j=0;j<s.length();j++){
+
+             if(2*(s.length()-j)<=ansr-ansl+1){
+                continue;
+            }
+             left=j;
+            right=j+1;
+            while(left>=0 && right<s.length() && s[left]==s[right]){
+                
+                left--;
+                right++;
+                
+            }
+            if(right-left-1>ansr-ansl+1){
+               
+                ansl=left+1;
+                ansr=right-1;  
+           }
+        }
+        string ans="";
+        for(int i=ansl;i<=ansr;i++)
+            ans+=s[i];
+        return ans;
     }
-   return s.substr(start, end);
-}
 };
