1c2afb9676deedee07d9b100de190820bdc63344
PRIYANSHI SHARMA <78258971+priyanshisharma01@users.noreply.github.com>
2022-08-22 16:49:14 +0530

    Time: 905 ms (20.10%), Space: 89.7 MB (81.45%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -1,61 +1,18 @@
-/*#include <unordered_map>
-#include <algorithm>
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        int count =0;
-        unordered_map<int,int> mp;
         sort(intervals.begin(),intervals.end());
-        for(int i=0;i<intervals.size()-1;i++)
+        int remove = 0, n = intervals.size(), end=intervals[0][1];
+        for(int i=1;i<n;i++)
         {
-        if(intervals[i][1]!=intervals[i+1][0])
-        {
-            count++;
-            intervals.erase(intervals.begin()+i);
-        }
-    }
-        for(int i=0;i<intervals.size();i++)
-        {
-            mp[intervals[i][0]]++;
-        }
-        auto iter = mp.begin();
-    while (iter != mp.end()) {
-        if(iter->second >1)
-        {
-            for(int j=1;j<iter->second;j++)
+            if(intervals[i][0]>=end)
+                end = intervals[i][1];
+            else
             {
-                count++;
-            }
-           
-        }
-        ++iter;
-    }
-    
-        return count;
-    }
-};
-*/
-
-
-bool mycmp(vector<int>&v1,vector<int>&v2)
-    {
-        return v1[1]<v2[1];
-    }
-class Solution {
-  
-public:
-    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-         if (intervals.size()==1) return 0;
-        sort(intervals.begin(),intervals.end(),mycmp);
-        int prevend=intervals[0][1];
-        int count=0;
-        for(int i=1;i<intervals.size();i++)
-        {
-            if(intervals[i][0]<prevend)count++;
-            else{
-                prevend=intervals[i][1];
+                remove++;
+                end = min(intervals[i][1],end);
             }
         }
-        return count;
+        return remove;
     }
 };
